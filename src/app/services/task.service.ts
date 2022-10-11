d34fb75f619b71f31fec1e09dd2cb8c5ca57b393
david-lafontant <lafontant.david@gmail.com>
2022-10-11 16:46:01 -0600

    Import mock task and create getTask method

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { TASKS } from '../mock-task';
+import { Task } from '../Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+
   constructor() { }
+
+  getTask(): Task[] {
+    return TASKS;
+  }
 }
