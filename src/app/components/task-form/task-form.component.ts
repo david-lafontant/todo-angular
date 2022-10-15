4f9b750efb51a6a380e0663da43b3125613624e0
david-lafontant <lafontant.david@gmail.com>
2022-10-15 14:51:00 -0600

    Setup onSubmit method in task-form component

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-task-form',
@@ -6,13 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent implements OnInit {
-  text: string = "";
-  day: string = "";
+  text: string ="";
+  day: string ="" ;
   reminder: boolean = false;
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  
+
+  onSubmit() {
+ if(this.text === ""){
+  return alert("Please enter task");
+ }
+ const newTask = {
+  text: this.text,
+  day: this.day,
+  reminder: this.reminder
+ }
+ this.onAddTask.emit(newTask);
+
+ 
+
+
+
+
+  this.text = "";
+  this.day = "";
+  this.reminder = false;
+
+  }
 }
