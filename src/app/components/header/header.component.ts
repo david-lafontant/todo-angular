47d4f2452f508261411cec2f5d889af732641bae
david-lafontant <lafontant.david@gmail.com>
2022-10-16 15:24:16 -0600

    Add uiService to header contructor

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   title: string = 'ToDo App';
   showAddTask: boolean;
   subscription: Subscription = new Subscription;
-  constructor() { }
+  constructor( private uiService: UiService) { }
 
   ngOnInit(): void {
   }
