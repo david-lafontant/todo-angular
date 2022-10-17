0957c8de933170c8c4850679d8c69ff9e8a239c3
david-lafontant <lafontant.david@gmail.com>
2022-10-16 19:03:50 -0600

    Setup task-form component constructor

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,47 +1,42 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
-import {UiService } from '../../services/ui.service';
+import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.css']
+  styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent implements OnInit {
-  text: string ="";
-  day: string ="" ;
+  text: string = '';
+  day: string = '';
   reminder: boolean = false;
   showAddTask!: boolean;
+  subscription: Subscription = new Subscription();
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
-  constructor(private uiService: UiService) { }
-
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value: boolean) => (this.showAddTask = value));
   }
 
-
-  
+  ngOnInit(): void { }
 
   onSubmit() {
- if(this.text === ""){
-  return alert("Please enter task");
- }
- const newTask = {
-  text: this.text,
-  day: this.day,
-  reminder: this.reminder
- }
- this.onAddTask.emit(newTask);
+    if (this.text === '') {
+      return alert('Please enter task');
+    }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+    this.onAddTask.emit(newTask);
 
- 
-
-
-
-
-  this.text = "";
-  this.day = "";
-  this.reminder = false;
-
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
 }
